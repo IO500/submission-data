3f551602e3787a755fccd8cbaa3ff358f3eeb63b
Jean Luca Bez <jeanlucabez@gmail.com>
2022-02-20 20:37:35 -0800

    add submission-44

---
+++ DDN/AI400/ExaScaler/2019/submission-44/io500.sh
@@ -0,0 +1,224 @@
+#!/bin/bash
+
+#LUSTRE_MDS=mds[11-14]
+LUSTRE_MDS=sv16[0-3],sv19[0-3]
+LUSTRE_OSS=sv16[0-3],sv19[0-3]
+LUSTRE_CLIENT=c0[82-83,85-87],c2[10,12-14],c208
+MGS=172.16.254.190@o2ib
+FSNAME=/es90
+MNT=/es90
+DSH=pdsh
+
+#PATH=/usr/mpi/gcc/openmpi-4.0.2a1/bin:$PATH
+#PATH=/work/bin/benchmark_bin/openmpi-3.1.3/bin:$PATH
+
+# Lustre Server Setting
+${DSH} -w ${LUSTRE_MDS} "echo 128 > /sys/module/mdt/parameters/max_mod_rpcs_per_client"
+${DSH} -w ${LUSTRE_MDS} lctl set_param \
+mdt.*.dom_lock=trylock
+
+${DSH} -w ${LUSTRE_OSS} "sysctl -w vm.min_free_kbytes=524288"
+${DSH} -w ${LUSTRE_OSS} lctl set_param \
+osd-ldiskfs.*.read_cache_enable=0 \
+obdfilter.*.writethrough_cache_enable=0 \
+obdfilter.*.brw_size=16 \
+obdfilter.*.precreate_batch=1024
+
+# ReMount Lustre Client
+${DSH} -w ${LUSTRE_CLIENT} umount -t lustre -a
+${DSH} -w ${LUSTRE_CLIENT} mount -t lustre ${MGS}:${FSNAME} ${MNT}
+sleep 2
+
+# Lustre Client Setting
+${DSH} -w ${LUSTRE_CLIENT} lctl set_param \
+osc.*.max_pages_per_rpc=16M \
+osc.*.max_rpcs_in_flight=16 \
+osc.*.max_dirty_mb=512 \
+osc.*.checksums=0 \
+llite.*.max_read_ahead_mb=2048 \
+ldlm.namespaces.*.lru_size=4000000 \
+mdc.*.max_rpcs_in_flight=128 \
+mdc.*.max_mod_rpcs_in_flight=127 \
+llite.*.max_read_ahead_per_file_mb=256
+sleep 2
+
+# Cleanup & TRIM to all OSTS
+${DSH} -w ${LUSTRE_CLIENT} lctl set_param ldlm.namespaces.*.lru_size=clear
+${DSH} -w ${LUSTRE_OSS} fstrim -av
+${DSH} -w ${LUSTRE_MDS},${LUSTRE_OSS} "echo 3 > /proc/sys/vm/drop_caches"
+${DSH} -w ${LUSTRE_CLIENT} "cpupower frequency-set -g performance"
+
+#
+# INSTRUCTIONS:
+# Edit this file as needed for your machine.
+# This simplified version is just for running on a single node.
+# It is a simplified version of the site-configs/sandia/startup.sh which include SLURM directives.
+# Most of the variables set in here are needed for io500_fixed.sh which gets sourced at the end of this.
+# Please also edit 'extra_description' function.
+
+set -euo pipefail  # better error handling
+
+# turn these to True successively while you debug and tune this benchmark.
+# for each one that you turn to true, go and edit the appropriate function.
+# to find the function name, see the 'main' function.
+# These are listed in the order that they run.
+io500_run_ior_easy="True" # does the write phase and enables the subsequent read
+io500_run_md_easy="True"  # does the creat phase and enables the subsequent stat
+io500_run_ior_hard="True" # does the write phase and enables the subsequent read
+io500_run_md_hard="True"  # does the creat phase and enables the subsequent read
+io500_run_find="True"
+io500_run_ior_easy_read="True"
+io500_run_md_easy_stat="True"
+io500_run_ior_hard_read="True"
+io500_run_md_hard_stat="True"
+io500_run_md_hard_read="True"
+io500_run_md_easy_delete="True" # turn this off if you want to just run find by itself
+io500_run_md_hard_delete="True" # turn this off if you want to just run find by itself
+io500_run_mdreal="False"  # this one is optional
+io500_cleanup_workdir="False"  # this flag is currently ignored. You'll need to clean up your data files manually if you want to.
+io500_stonewall_timer=300 # Stonewalling timer, stop with wearout after 300s with default test, set to 0, if you never want to abort...
+
+# to run this benchmark, find and edit each of these functions.
+# please also edit 'extra_description' function to help us collect the required data.
+function main {
+  setup_directories
+  setup_paths
+  setup_ior_easy # required if you want a complete score
+  setup_ior_hard # required if you want a complete score
+  setup_mdt_easy # required if you want a complete score
+  setup_mdt_hard # required if you want a complete score
+  setup_find     # required if you want a complete score
+  setup_mdreal   # optional
+  run_benchmarks
+}
+
+function setup_directories {
+  # set directories for where the benchmark files are created and where the results will go.
+  # If you want to set up stripe tuning on your output directories or anything similar, then this is good place to do it.
+  timestamp=`date +%Y.%m.%d-%H.%M.%S`           # create a uniquifier
+  #io500_workdir=$PWD/datafiles/io500.$timestamp # directory where the data will be stored
+  io500_workdir=${MNT}/io500.out
+  io500_result_dir=$PWD/results/$timestamp      # the directory where the output results will be kept
+  mkdir -p $io500_workdir $io500_result_dir
+  mkdir -p $io500_workdir/ior_hard $io500_workdir/ior_easy
+  lfs setdirstripe -c 8 $io500_workdir/mdt_easy
+  lfs setdirstripe -c 8 $io500_workdir/mdt_hard
+  lfs setdirstripe -c 8 -D $io500_workdir/mdt_easy
+  lfs setdirstripe -c 8 -D $io500_workdir/mdt_hard
+
+  lfs setstripe -L mdt -E 1M $io500_workdir/mdt_easy
+  #lfs setstripe -L mdt -E 1M $io500_workdir/mdt_hard
+  lfs setstripe -C 320 -S 16M $io500_workdir/ior_hard
+}
+
+function setup_paths {
+  # Set the paths to the binaries.  If you ran ./utilities/prepare.sh successfully, then binaries are in ./bin/
+  io500_ior_cmd=$PWD/bin/ior
+  io500_mdtest_cmd=$PWD/bin/mdtest
+  io500_mdreal_cmd=$PWD/bin/md-real-io
+  io500_mpirun="/work/home/sihara/mpi/mpich-3.3/bin/mpirun"
+  io500_mpiargs="-np 160 -hostfile hostfile -ppn 16"
+}
+
+function setup_ior_easy {
+  # io500_ior_easy_size is the amount of data written per rank in MiB units,
+  # but it can be any number as long as it is somehow used to scale the IOR
+  # runtime as part of io500_ior_easy_params
+  io500_ior_easy_size=120000
+  # 2M writes, 2 GB per proc, file per proc
+  io500_ior_easy_params="-t 2048k -b ${io500_ior_easy_size}m -F"
+}
+
+function setup_mdt_easy {
+  io500_mdtest_easy_params="-u -L" # unique dir per thread, files only at leaves
+  io500_mdtest_easy_files_per_proc=500000
+}
+
+function setup_ior_hard {
+  io500_ior_hard_writes_per_proc=230000
+  io500_ior_hard_other_options="" #e.g., -E to keep precreated files using lfs setstripe, or -a MPIIO
+}
+
+function setup_mdt_hard {
+  io500_mdtest_hard_files_per_proc=81500
+  io500_mdtest_hard_other_options=""
+}
+
+function setup_find {
+  #
+  # setup the find command. This is an area where innovation is allowed.
+  #    There are three default options provided. One is a serial find, one is python
+  #    parallel version, one is C parallel version.  Current default is to use serial.
+  #    But it is very slow. We recommend to either customize or use the C parallel version.
+  #    For GPFS, we recommend to use the provided mmfind wrapper described below.
+  #    Instructions below.
+  #    If a custom approach is used, please provide enough info so others can reproduce.
+
+  # the serial version that should run (SLOWLY) without modification
+  #io500_find_mpi="False"
+  #io500_find_cmd=$PWD/bin/sfind.sh
+  #io500_find_cmd_args=""
+
+  # a parallel version in C, the -s adds a stonewall
+  #   for a real run, turn -s (stonewall) off or set it at 300 or more
+  #   to prepare this (assuming you've run ./utilities/prepare.sh already):
+  #   > cd build/pfind
+  #   > ./prepare.sh
+  #   > ./compile.sh
+  #   > cp pfind ../../bin/
+  #   If you use io500_find_mpi="True", then this will run with the same
+  #   number of MPI nodes and ranks as the other phases.
+  #   If you prefer another number, and fewer might be better here,
+  #   Then you can set io500_find_mpi to be "False" and write a wrapper
+  #   script for this which sets up MPI as you would like.  Then change
+  #   io500_find_cmd to point to your wrapper script.
+  io500_find_mpi="True"
+  io500_find_cmd="$PWD/bin/pfind"
+  # uses stonewalling, run pfind 
+  io500_find_cmd_args="-q 200000 -s $io500_stonewall_timer -r $io500_result_dir/pfind_results"
+
+  # for GPFS systems, you should probably use the provided mmfind wrapper
+  # if you used ./utilities/prepare.sh, you'll find this wrapper in ./bin/mmfind.sh
+  #io500_find_mpi="False"
+  #io500_find_cmd="$PWD/bin/mmfind.sh"
+  #io500_find_cmd_args=""
+}
+
+function setup_mdreal {
+  io500_mdreal_params="-P=5000 -I=1000"
+}
+
+function run_benchmarks {
+  # Important: source the io500_fixed.sh script.  Do not change it. If you discover
+  # a need to change it, please email the mailing list to discuss
+  source ./utilities/io500_fixed.sh 2>&1 | tee $io500_result_dir/io-500-summary.$timestamp.txt
+}
+
+# Add key/value pairs defining your system
+# Feel free to add extra ones if you'd like
+function extra_description {
+  # top level info
+  io500_info_system_name='Bancholab'      # e.g. Oakforest-PACS
+  io500_info_institute_name='DDN'   # e.g. JCAHPC
+  io500_info_storage_age_in_months='2' # not install date but age since last refresh
+  io500_info_storage_install_date='05/19'  # MM/YY
+  io500_info_filesystem='Lustre'     # e.g. BeeGFS, DataWarp, GPFS, IME, Lustre
+  io500_info_filesystem_version='2.13'
+  io500_info_filesystem_vendor='DDN'
+  # client side info
+  io500_info_num_client_nodes='10'
+  io500_info_procs_per_node='16'
+  # server side info
+  io500_info_num_metadata_server_nodes='8'
+  io500_info_num_data_server_nodes='8'
+  io500_info_num_data_storage_devices='40'  # if you have 5 data servers, and each has 5 drives, then this number is 25
+  io500_info_num_metadata_storage_devices='8'  # if you have 2 metadata servers, and each has 5 drives, then this number is 10
+  io500_info_data_storage_type='SSD' # HDD, SSD, persistent memory, etc, feel free to put specific models
+  io500_info_metadata_storage_type='SSD' # HDD, SSD, persistent memory, etc, feel free to put specific models
+  io500_info_storage_network='infiniband' # infiniband, omnipath, ethernet, etc
+  io500_info_storage_interface='NVMe' # SAS, SATA, NVMe, etc
+  # miscellaneous
+  io500_info_whatever='WhateverElseYouThinkRelevant'
+}
+
+main

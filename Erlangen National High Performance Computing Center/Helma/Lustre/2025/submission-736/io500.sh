efdeda5cc16b774e2f62ef8e072792a5e82a7546
Jean Luca Bez <jeanlucabez@gmail.com>
2026-02-18 08:28:10 -0800

    add submission-736

---
+++ Erlangen National High Performance Computing Center/Helma/Lustre/2025/submission-736/io500.sh
@@ -0,0 +1,140 @@
+#!/bin/bash
+#SBATCH --nodes=10 --ntasks-per-node=6 -p compute -A ku0598
+
+#. /anvme/megware/hnvme/hpcx-v2.22.1-gcc-inbox-redhat9-cuda12-x86_64/hpcx-init.sh
+#hpcx_load
+
+eval `tclsh /apps/modules/modulecmd.tcl bash autoinit`
+[ -z $MANPATH ] && export MANPATH=:
+# add hpc-workspace to path and manpath
+export PATH=${PATH}:/apps/hpc-workspace/1.5.0/bin
+export MANPATH=${MANPATH}:/apps/hpc-workspace/1.5.0/share/man
+ml load gcc/14.2.0
+ml load openmpi/5.0.5-gcc14.2.0-cuda
+
+# INSTRUCTIONS:
+#
+# The only parts of the script that may need to be modified are:
+#  - setup() to configure the binary locations and MPI parameters
+# Please visit https://vi4io.org/io500-info-creator/ to help generate the
+# "system-information.txt" file, by pasting the output of the info-creator.
+# This file contains details of your system hardware for your submission.
+
+# This script takes its parameters from the same .ini file as io500 binary.
+io500_ini="$1"          # You can set the ini file here
+io500_mpirun="$(which mpirun) --allow-run-as-root -x LD_LIBRARY_PATH --mca plm_rsh_no_tree_spawn true --mca plm_rsh_num_concurrent 200 --hostfile hostfile"
+io500_mpiargs="--map-by ppr:50:package --bind-to core -mca coll_hcoll_enable 0 -x HCOLL_ENABLE_MCAST_ALL=0 -x UCX_NET_DEVICES=mlx5_2:1,mlx5_3:1,mlx5_4:1,mlx5_5:1 -x UCX_LOG_LEVEL=WARN"
+
+function setup(){
+  local workdir="$1"
+  local resultdir="$2"
+  mkdir -p $workdir $resultdir
+
+  lfs setstripe -c 1 $workdir
+  mkdir $workdir/ior-easy $workdir/ior-hard $workdir/ior-rnd1MB $workdir/ior-rnd4K
+  mkdir $workdir/mdtest-easy $workdir/mdtest-hard $workdir/mdworkbench
+  # Example commands to create output directories for Lustre.  Creating
+  # top-level directories is allowed, but not the whole directory tree.
+  if (( $(lfs df $workdir | grep -c MDT) > 1 )); then
+    #lfs setdirstripe -D -c -1 $workdir/mdtest-easy
+    lfs setdirstripe -D --max-inherit-rr 2 $workdir/mdtest-easy
+    lfs setdirstripe -D -c -1 $workdir/mdtest-hard
+    #lfs setdirstripe -D -c -1 $workdir/mdworkbench
+    lfs setdirstripe -D --max-inherit-rr 5 $workdir/mdworkbench
+  fi
+  lfs setstripe -c 1 -S 16M $workdir/ior-easy
+  local osts=$(lfs df $workdir | grep -c OST)
+  # Try overstriping for ior-hard to improve scaling, or use wide striping
+  lfs setstripe -C $((osts * 5)) -S 16M $workdir/ior-hard
+  lfs setstripe -c $osts -S 16M $workdir/ior-rnd1MB
+  lfs setstripe -c $osts -S 16M $workdir/ior-rnd4K
+  # Try to use DoM if available, otherwise use default for small files
+  lfs setstripe -E 1M -L mdt $workdir/mdtest-easy || true #DoM?
+  lfs setstripe -E 1M -L mdt $workdir/mdtest-hard || true #DoM?
+  lfs setstripe -E 1M -L mdt $workdir/mdworkbench
+}
+
+# *****  YOU SHOULD NOT EDIT ANYTHING BELOW THIS LINE  *****
+set -eo pipefail  # better error handling
+
+if [[ -z "$io500_ini" ]]; then
+  echo "error: ini file must be specified.  usage: $0 <config.ini>"
+  exit 1
+fi
+if [[ ! -s "$io500_ini" ]]; then
+  echo "error: ini file '$io500_ini' not found or empty"
+  exit 2
+fi
+
+function get_ini_section_param() {
+  local section="$1"
+  local param="$2"
+  local inside=false
+
+  while read LINE; do
+    LINE=$(sed -e 's/ *#.*//' -e '1s/ *= */=/' <<<$LINE)
+    $inside && [[ "$LINE" =~ "[.*]" ]] && inside=false && break
+    [[ -n "$section" && "$LINE" =~ "[$section]" ]] && inside=true && continue
+    ! $inside && continue
+    #echo $LINE | awk -F = "/^$param/ { print \$2 }"
+    if [[ $(echo $LINE | grep "^$param *=" ) != "" ]] ; then
+      # echo "$section : $param : $inside : $LINE" >> parsed.txt # debugging
+      echo $LINE | sed -e "s/[^=]*=[ \t]*\(.*\)/\1/"
+      return
+    fi
+  done < $io500_ini
+  echo ""
+}
+
+function get_ini_global_param() {
+  local param="$1"
+  local default="$2"
+  local val
+
+  val=$(get_ini_section_param global $param |
+  	sed -e 's/[Ff][Aa][Ll][Ss][Ee]/False/' -e 's/[Tt][Rr][Uu][Ee]/True/')
+
+  echo "${val:-$default}"
+}
+
+function run_benchmarks {
+  #$io500_mpirun $io500_mpiargs $PWD/io500 $io500_ini --timestamp $timestamp --mode=extended
+  $io500_mpirun $io500_mpiargs $PWD/io500 $io500_ini --timestamp $timestamp
+}
+
+create_tarball() {
+  local sourcedir=$(dirname $io500_resultdir)
+  local fname=$(basename ${io500_resultdir})
+  local tarball=$sourcedir/io500-$HOSTNAME-$fname.tgz
+
+  cp -v $0 $io500_ini $io500_resultdir
+  tar czf $tarball -C $sourcedir $fname
+  echo "Created result tarball $tarball"
+}
+
+function main {
+  # These commands extract the 'datadir' and 'resultdir' from .ini file
+  timestamp=$(date +%Y.%m.%d-%H.%M.%S)           # create a uniquifier
+  [ $(get_ini_global_param timestamp-datadir True) != "False" ] &&
+    ts="$timestamp" || ts="io500"
+  # working directory where the test files will be created
+  export io500_workdir=$(get_ini_global_param datadir $PWD/datafiles)/$ts
+  [ $(get_ini_global_param timestamp-resultdir True) != "False" ] &&
+    ts="$timestamp" || ts="io500"
+  # the directory where the output results will be kept
+  export io500_resultdir=$(get_ini_global_param resultdir $PWD/results)/$ts
+
+  setup $io500_workdir $io500_resultdir
+  run_benchmarks
+
+  if [[ ! -s "system-information.txt" ]]; then
+    echo "Warning: please create a 'system-information.txt' description by"
+    echo "copying the information from https://vi4io.org/io500-info-creator/"
+  else
+    cp "system-information.txt" $io500_resultdir
+  fi
+
+  create_tarball
+}
+
+main

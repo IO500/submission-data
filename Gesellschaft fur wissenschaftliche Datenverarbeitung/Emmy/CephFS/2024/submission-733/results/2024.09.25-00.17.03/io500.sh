68172a835210ba8b46acc4bbc7d561b95276d34d
Jean Luca Bez <jeanlucabez@gmail.com>
2026-02-18 08:28:10 -0800

    add submission-733

---
+++ Gesellschaft fur wissenschaftliche Datenverarbeitung/Emmy/CephFS/2024/submission-733/results/2024.09.25-00.17.03/io500.sh
@@ -0,0 +1,146 @@
+#!/bin/bash
+#SBATCH --nodes=10 --ntasks-per-node=6 -p compute -A ku0598
+
+# INSTRUCTIONS:
+#
+# The only parts of the script that may need to be modified are:
+#  - setup() to configure the binary locations and MPI parameters
+# Please visit https://vi4io.org/io500-info-creator/ to help generate the
+# "system-information.txt" file, by pasting the output of the info-creator.
+# This file contains details of your system hardware for your submission.
+
+# This script takes its parameters from the same .ini file as io500 binary.
+io500_ini="$1"          # You can set the ini file here
+io500_mpirun="mpiexec"
+io500_mpiargs="-npernode 15 --hostfile hosts"
+
+function setup(){
+  local workdir="$1"
+  local resultdir="$2"
+  mkdir -p $workdir $resultdir
+
+  mkdir $workdir/ior-easy $workdir/ior-hard
+  mkdir $workdir/mdtest-easy $workdir/mdtest-hard
+
+  # Make the easy test directories in advance, technically violates the rules
+  # but allows us to set the xattr to use pinning.
+  MAX_MDS=28
+  MPI_RANKS=270
+
+  mkdir -p $io500_workdir/mdtest-easy/test-dir.0-0
+
+# N RR
+#  RR_START=0
+
+# N-1 RR
+  RR_START=1
+
+  setfattr -n ceph.dir.pin -v 0 $io500_workdir/mdtest-easy/test-dir.0-0
+  MOD=$(( ${MAX_MDS} - ${RR_START} ))
+  MAXRANK=$(( ${MPI_RANKS} ))
+  for (( RANK=0; RANK<${MAXRANK}; RANK++ ))
+  do
+    n=0
+    if [ ${MOD} -gt 0 ]
+    then
+      n=$(( ${RANK}%${MOD} + ${RR_START} ))
+    fi
+    mkdir -p $io500_workdir/mdtest-easy/test-dir.0-0/mdtest_tree.${RANK}.0
+    echo "MPI rank ${RANK} assigned to mds rank ${n}"
+    setfattr -n ceph.dir.pin -v ${n} $io500_workdir/mdtest-easy/test-dir.0-0/mdtest_tree.${RANK}.0
+  done
+
+  # For the easy test, use cephfs ephemeral pinning
+#  sudo setfattr -n ceph.dir.pin.distributed -v 1 $io500_workdir/mdtest-easy/test-dir.0-0
+#  sudo setfattr -n ceph.dir.pin.random -v 1.0 $io500_workdir/mdtest-easy
+
+  # For ior-hard, pin to rank 0
+  setfattr -n ceph.dir.pin -v 0 $io500_workdir/ior-hard
+
+}
+
+# *****  YOU SHOULD NOT EDIT ANYTHING BELOW THIS LINE  *****
+set -eo pipefail  # better error handling
+
+if [[ -z "$io500_ini" ]]; then
+  echo "error: ini file must be specified.  usage: $0 <config.ini>"
+  exit 1
+fi
+if [[ ! -s "$io500_ini" ]]; then
+  echo "error: ini file '$io500_ini' not found or empty"
+  exit 2
+fi
+
+function get_ini_section_param() {
+  local section="$1"
+  local param="$2"
+  local inside=false
+
+  while read LINE; do
+    LINE=$(sed -e 's/ *#.*//' -e '1s/ *= */=/' <<<$LINE)
+    $inside && [[ "$LINE" =~ "[.*]" ]] && inside=false && break
+    [[ -n "$section" && "$LINE" =~ "[$section]" ]] && inside=true && continue
+    ! $inside && continue
+    #echo $LINE | awk -F = "/^$param/ { print \$2 }"
+    if [[ $(echo $LINE | grep "^$param *=" ) != "" ]] ; then
+      # echo "$section : $param : $inside : $LINE" >> parsed.txt # debugging
+      echo $LINE | sed -e "s/[^=]*=[ \t]*\(.*\)/\1/"
+      return
+    fi
+  done < $io500_ini
+  echo ""
+}
+
+function get_ini_global_param() {
+  local param="$1"
+  local default="$2"
+  local val
+
+  val=$(get_ini_section_param global $param |
+  	sed -e 's/[Ff][Aa][Ll][Ss][Ee]/False/' -e 's/[Tt][Rr][Uu][Ee]/True/')
+
+  echo "${val:-$default}"
+}
+
+function run_benchmarks {
+  $io500_mpirun $io500_mpiargs $PWD/io500 $io500_ini --timestamp $timestamp
+}
+
+create_tarball() {
+  local sourcedir=$(dirname $io500_resultdir)
+  local fname=$(basename ${io500_resultdir})
+  local tarball=$sourcedir/io500-$HOSTNAME-$fname.tgz
+
+  cp -v $0 $io500_ini $io500_resultdir
+  tar czf $tarball -C $sourcedir $fname
+  echo "Created result tarball $tarball"
+}
+
+function main {
+  # These commands extract the 'datadir' and 'resultdir' from .ini file
+  timestamp=$(date +%Y.%m.%d-%H.%M.%S)           # create a uniquifier
+  [ $(get_ini_global_param timestamp-datadir True) != "False" ] &&
+    ts="$timestamp" || ts="io500"
+  # working directory where the test files will be created
+  export io500_workdir=$(get_ini_global_param datadir $PWD/datafiles)/$ts
+  [ $(get_ini_global_param timestamp-resultdir True) != "False" ] &&
+    ts="$timestamp" || ts="io500"
+  # the directory where the output results will be kept
+  export io500_resultdir=$(get_ini_global_param resultdir $PWD/results)/$ts
+
+  setup $io500_workdir $io500_resultdir
+  run_benchmarks
+
+  if [[ ! -s "system-information.txt" ]]; then
+    echo "Warning: please create a 'system-information.txt' description by"
+    echo "copying the information from https://vi4io.org/io500-info-creator/"
+  else
+    cp "system-information.txt" $io500_resultdir
+  fi
+
+  create_tarball
+}
+
+main
+
+

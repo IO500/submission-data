d43347bc0e688109cc10b2cf2b40b6c07af0cb71
Jean Luca Bez <jeanlucabez@gmail.com>
2026-02-18 08:28:13 -0800

    add submission-784

---
+++ IBM Tucson/Saguaro Storage Scale System 6000/IBM Storage Scale 6.0.0.0 with ESS 7.0.0.0 (early pre-GA build)/2025/submission-784/run_1_io500.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+./io500_isc24.sh config_withFGWS_PFIND_FGRS.bb8nvme16m.ini yellowstone_nodesib.10 20
+exit 0
+
+#host.list below
+cat nodesib.10
+saguaro1ib
+saguaro2ib
+saguaro3ib
+saguaro4ib
+saguaro5ib
+saguaro6ib
+saguaro7ib
+saguaro8ib
+saguaro9ib
+saguaro10ib
